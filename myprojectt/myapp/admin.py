7184c390ce0858cd5d4aefe61c69b677692e7c0c
zhakonbekk <zhatbak@gmail.com>
2025-04-02 15:00:04 +0500

    Added authentication

--- myprojectt/myapp/admin.py
+++ myprojectt/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+from .models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # Поля, которые будут отображаться в списке
+    search_fields = ('title',)  # Поиск по названию задачи
+    list_filter = ('created_at',)  # Фильтр по дате создания
+
 # Register your models here.
--- myprojectt/myapp/models.py
+++ myprojectt/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Task(models.Model):
+    title = models.CharField(max_length=255) # Название задачи
+    description = models.TextField(blank=True, null=True) # Описание
+    created_at = models.DateTimeField(auto_now_add=True) # Дата создания
+
+def __str__(self):
+    return self.title
+
+
 # Create your models here.
